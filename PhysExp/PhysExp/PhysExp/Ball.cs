f7138cca96452ffc94707f1c237570cee92c5562
stopanko <stepander007@gmail.com>
2014-01-28 02:25:06 +0200

    Add ball class to Phys Exp

---
+++ PhysExp/PhysExp/PhysExp/Ball.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Net;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
+
+using System.Linq;
+using System.Collections.Generic;
+
+//using System.Windows.Ink;
+//using System.Windows.Media; // якщо додати то тре буде писати Microsoft.Xna.Framework.Color
+//using System.Windows.Media.Animation;
+
+//using Microsoft.Phone.Controls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input.Touch;
+
+
+//using Microsoft.Xna.Framework.Graphics.PackedVector;
+//AppWarp
+//using Newtonsoft.Json;
+//using Newtonsoft.Json.Linq;
+//using com.shephertz.app42.gaming.multiplayer.client.events;
+//using com.shephertz.app42.gaming.multiplayer.client.command;
+//using com.shephertz.app42.gaming.multiplayer.client;
+
+//
+//farseer
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using FarseerPhysics.Common;
+//
+
+namespace PhysExp
+{
+    public class Ball
+    {
+        //ball settings
+        private Body _circleBody;
+        private Texture2D _circleSprite;
+        private Vector2 _circleCenter; // центр текстури для методу Draw
+        private float _speed = 48;
+        private Vector2 _clickPos = new Vector2();
+        private Vector2 _direction = new Vector2();
+        //
+        public World _world = new World(new Vector2(0, 0));
+        public Vector2 SpritePos = new Vector2(0, 0);
+        
+        
+        //float speed = 5f;
+
+        string[] Mas = new string[2];
+
+        //public static Vector2[] cPos;
+        //public static Vector2[] sPos;
+        //public static int masItem; 
+
+
+        //List<JObject> ballsList = new List<JObject>();
+
+        //List<Vector2> Ballslist = new List<Vector2>();
+
+        public void InitBallContent(ContentManager Content)
+        {
+            //Content.RootDirectory = "Content";
+            _circleSprite = Content.Load<Texture2D>("CircleSprite"); //текстура
+            _circleCenter = ConvertUnits.ToSimUnits(new Vector2(_circleSprite.Width / 2f, _circleSprite.Height / 2f));//знаходимо центр текстури
+            _circleBody = BodyFactory.CreateCircle(_world, ConvertUnits.ToSimUnits(_circleSprite.Height / 2f), 1f, ConvertUnits.ToSimUnits(new Vector2(50, 50))); // (світ, радіус(визнач за текстурою), шось,початкова позиція тіла)
+            _circleBody.BodyType = BodyType.Dynamic;
+            _circleBody.Restitution = 0.8f;
+            _circleBody.Friction = 0.5f;
+        }
+
+        public void GetMoveDir(Vector2 ClickPos)//передамо вектор кліку
+        {
+            //Vector2 ClickPos = new Vector2(loc.X, loc.Y);
+            Vector2 DiffDist = ClickPos - ConvertUnits.ToDisplayUnits(_circleBody.Position); // шукає різницю між позицією кліку та позицією шару для того, щоб побудувати вектор в напрямку кліку з початку коордтнат.
+            //double distance = Math.Sqrt(Math.Pow(_circleBody.Position.X - ClickPos.X, 2) + Math.Pow(_circleBody.Position.Y - ClickPos.Y, 2));
+            double Distance = Math.Sqrt(Math.Pow(DiffDist.X, 2) + Math.Pow(DiffDist.Y, 2));//  дистанція нового кліку
+            double match = Distance / _speed; // взнаємо в скільки раз відстань між шаром та кліком менша від потрібної швидкості
+            _direction = ConvertUnits.ToSimUnits(new Vector2((float)(DiffDist.X / match), (float)(DiffDist.Y / match)));
+                   
+        }
+
+
+        public void MoveBall()
+        {
+
+            //SpritePos += _direction * (float)e.ElapsedTime.Milliseconds / 30 * speed * (float)e.ElapsedTime.Milliseconds / 30;
+            _circleBody.LinearVelocity = _direction;// нажаємо прискорення тілу
+        }
+
+        //public void SendDates()
+        //{
+            
+        //    JObject sendObj = new JObject();
+        //    sendObj.Add("X", this._clickPos.X);
+        //    sendObj.Add("Y", this._clickPos.Y);
+        //    sendObj.Add("Item", GamePage.masItem);
+            
+        //    WarpClient.GetInstance().SendUpdatePeers(System.Text.Encoding.UTF8.GetBytes(sendObj.ToString()));//sendObj.ToString()));
+        //}
+
+
+        //public void GetDates(UpdateEvent eventObj)
+        //{
+
+        //    JObject jsonObj = JObject.Parse(System.Text.Encoding.UTF8.GetString(eventObj.getUpdate(), 0, eventObj.getUpdate().Length));
+        //    _clickPos.X = int.Parse(jsonObj["X"].ToString());
+        //    _clickPos.Y = int.Parse(jsonObj["Y"].ToString());
+        //    this.GetMoveDir(_clickPos);       
+ 
+        //}
+
+        public void DrawBall(SpriteBatch spriteBatch)
+        {
+            //spriteBatch.Begin();
+            spriteBatch.Draw(_circleSprite, ConvertUnits.ToDisplayUnits(_circleBody.Position), null, Color.White, ConvertUnits.ToDisplayUnits(_circleBody.Rotation), ConvertUnits.ToDisplayUnits(_circleCenter), 1f, SpriteEffects.None, 0f);
+            
+            //                спрайт            позиція тіла                             обертання тіла     центер текстури(зміщуємо верхній лівий кут в центр)
+            
+            //spriteBatch.End();
+        }
+    }
+}
--- PhysExp/PhysExp/PhysExp/Game1.cs
+++ PhysExp/PhysExp/PhysExp/Game1.cs
@@ -24,23 +24,23 @@ namespace PhysExp
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        float halfWidth;
-        float halfHeight;
+        //float halfWidth;
+        //float halfHeight;
         float width;
         float height;
         float rad;
-        private World _world;
-        
+        public static World _world;
+        Ball b = new Ball();
         private Body _circleBody;
         private Body borderBody;
-        private Body _circleBody2;
+        //private Body _circleBody2;
         private Texture2D _circleSprite;
         private Texture2D _1;
         private Vector2 _1Center;
         private Vector2 _circleCenter;
 
 
-        Vector2 Direction;
+        //Vector2 Direction;
         SpriteFont Font;
         string Text = "";
         public Game1()
@@ -69,8 +69,8 @@ namespace PhysExp
             ConvertUnits.SetDisplayUnitToSimUnitRatio(64f);
             width = ConvertUnits.ToSimUnits(800);
             height = ConvertUnits.ToSimUnits(480);
-            //halfWidth = ConvertUnits.ToSimUnits(800);
-            //halfHeight = ConvertUnits.ToSimUnits(480);
+           
+            
             base.Initialize();
         }
 
@@ -82,29 +82,26 @@ namespace PhysExp
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            
-            Font = Content.Load<SpriteFont>("Font1");
+            b.InitBallContent(Content);
+            ///////////////////
+            //Font = Content.Load<SpriteFont>("Font1");
 
-            _world = new World(new Vector2(1, 0));
+            //_world = new World(new Vector2(1, 0));
              
-            _circleSprite = Content.Load<Texture2D>("CircleSprite");//завантажуємо текстуру
-            _1 = Content.Load<Texture2D>("1");
-            _circleCenter = ConvertUnits.ToSimUnits(new Vector2(_circleSprite.Width / 2f, _circleSprite.Height / 2f));//знаходимо центр текстури
-            _1Center = ConvertUnits.ToSimUnits(new Vector2(_1.Height / 2, _1.Width / 2));
-            rad = ConvertUnits.ToSimUnits(_circleSprite.Height / 2f);
-            _circleBody = BodyFactory.CreateCircle(_world, ConvertUnits.ToSimUnits(_circleSprite.Height / 2f), 1f, ConvertUnits.ToSimUnits (new Vector2(10, 10))); // (світ, радіус(визнач за текстурою), шось,початкова позиція тіла)
-            _circleBody.BodyType = BodyType.Dynamic;
-            _circleBody.Restitution = 0.8f;
-            _circleBody.Friction = 0.5f;
-            
+            //_circleSprite = Content.Load<Texture2D>("CircleSprite");//завантажуємо текстуру
+            //_1 = Content.Load<Texture2D>("1");
+            //_circleCenter = ConvertUnits.ToSimUnits(new Vector2(_circleSprite.Width / 2f, _circleSprite.Height / 2f));//знаходимо центр текстури
+            //_1Center = ConvertUnits.ToSimUnits(new Vector2(_1.Height / 2, _1.Width / 2));
+            //rad = ConvertUnits.ToSimUnits(_circleSprite.Height / 2f);
+            //_circleBody = BodyFactory.CreateCircle(_world, ConvertUnits.ToSimUnits(_circleSprite.Height / 2f), 1f, ConvertUnits.ToSimUnits (new Vector2(10, 10))); // (світ, радіус(визнач за текстурою), шось,початкова позиція тіла)
+            //_circleBody.BodyType = BodyType.Dynamic;
+            //_circleBody.Restitution = 0.8f;
+            //_circleBody.Friction = 0.5f;
+            ///////////////////////////////////
+
             // TODO: use this.Content to load your game content here
 
 
-
-            //_circleBody2 = BodyFactory.CreateCircle(_world, ConvertUnits.ToSimUnits(_circleSprite.Height / 2f), 1f, ConvertUnits.ToSimUnits(new Vector2(150, 120))); // (світ, радіус(визнач за текстурою), шось,початкова позиція тіла)
-            //_circleBody2.BodyType = BodyType.Dynamic;
-            //_circleBody2.Restitution = 0.8f;
-            //_circleBody2.Friction = 0.5f;
 
             Vertices borders = new Vertices(4);
             borders.Add(new Vector2(0 , 0 ));
@@ -113,7 +110,7 @@ namespace PhysExp
             borders.Add(new Vector2(0 , height));
 
             
-            borderBody = BodyFactory.CreateLoopShape(_world, borders);
+            borderBody = BodyFactory.CreateLoopShape(b._world, borders);
             borderBody.CollisionCategories = Category.All;
             borderBody.CollidesWith = Category.All;
         }
@@ -145,53 +142,27 @@ namespace PhysExp
             {
                 if (loc.State == TouchLocationState.Pressed)
                 {
-                    int speed = 48;
-                    Vector2 ClickPos = new Vector2(loc.Position.X, loc.Position.Y);
-                    Vector2 DiffDist = ClickPos - ConvertUnits.ToDisplayUnits(_circleBody.Position); // шукає різницю між позицією кліку та позицією шару для того, щоб побудувати вектор в напрямку кліку з початку коордтнат.
-                    //double distance = Math.Sqrt(Math.Pow(_circleBody.Position.X - ClickPos.X, 2) + Math.Pow(_circleBody.Position.Y - ClickPos.Y, 2));
-                    double Distance = Math.Sqrt(Math.Pow(DiffDist.X, 2) + Math.Pow(DiffDist.Y, 2));//  дистанція нового кліку
-                    double match = Distance / speed; // взнаємо в скільки раз відстань між шаром та кліком менша від потрібної швидкості
-                    //Direction = ClickPos - _circleBody.Position;
+                    b.GetMoveDir(new Vector2(loc.Position.X, loc.Position.Y));
+                    b.MoveBall();
+                    //////////////
+                    //int speed = 48;
+                    //Vector2 ClickPos = new Vector2(loc.Position.X, loc.Position.Y);
+                    //Vector2 DiffDist = ClickPos - ConvertUnits.ToDisplayUnits(_circleBody.Position); // шукає різницю між позицією кліку та позицією шару для того, щоб побудувати вектор в напрямку кліку з початку коордтнат.
+                    ////double distance = Math.Sqrt(Math.Pow(_circleBody.Position.X - ClickPos.X, 2) + Math.Pow(_circleBody.Position.Y - ClickPos.Y, 2));
+                    //double Distance = Math.Sqrt(Math.Pow(DiffDist.X, 2) + Math.Pow(DiffDist.Y, 2));//  дистанція нового кліку
+                    //double match = Distance / speed; // взнаємо в скільки раз відстань між шаром та кліком менша від потрібної швидкості
                     
-                    //Direction.Normalize();
-                    //int Xdirection = 1;
-                    //int Ydirection = 1;
-                    //_circleBody.ApplyLinearImpulse(ConvertUnits.ToSimUnits(Direction));
-                    //_circleBody.ApplyLinearImpulse(new Vector2(20000, 0));
-                    //_circleBody.ApplyForce(new Vector2(20000, 0));
-                    //_circleBody.Position = ConvertUnits.ToSimUnits(loc.Position);
-                    //_circleBody.ApplyTorque(10);
-                    //_circleBody.ApplyLinearImpulse(new Vector2(10, -10f)); //прикладає імпульс(сумується з наступним)
-                    //_circleBody.ApplyForce(new Vector2(10, -0f)); //прикладає силу(також сумується)
-                    //if (ClickPos.X < ConvertUnits.ToDisplayUnits(_circleBody.Position.X))
-                    //{
-                    //    Xdirection = -1;
-                    //}
-                    //else
-                    //{
-                    //    Xdirection = 1;
-                    //}
-                    //if (ClickPos.Y < ConvertUnits.ToDisplayUnits(_circleBody.Position.Y))
-                    //{
-                    //    Ydirection = -1;
-                    //}
-                    //else
-                    //{
-                    //    Ydirection = 1;
-                    //}
-                    //_circleBody.LinearVelocity = ConvertUnits.ToSimUnits(new Vector2((float)((ClickPos.X - ConvertUnits.ToDisplayUnits(_circleBody.Position.X)) / match) * Xdirection, (float)((ClickPos.Y - ConvertUnits.ToDisplayUnits(_circleBody.Position.Y)) / match) * Ydirection));
-                    // швидкість об'єкта
-                    _circleBody.LinearVelocity = ConvertUnits.ToSimUnits(new Vector2((float)(DiffDist.X / match), (float)(DiffDist.Y / match)));
-                    //_circleBody.IsBullet = false;//ConvertUnits.ToSimUnits(20);
-                    //Text = ConvertUnits.ToDisplayUnits(Direction).ToString();
-                    Text = ClickPos.ToString() + "\n" + Distance.ToString() + " " + match.ToString() + "\n" + ((float)(ClickPos.X / match)).ToString() + " " + ((float)(ClickPos.Y / match)).ToString(); //loc.Position.ToString() + "Vids: " + match.ToString() + "V raz: " + (match / 84).ToString() + "\n" + "Nklic:" + new Vector2((float)(loc.Position.X / (match / 84)), (float)(loc.Position.Y / (match / 84))).ToString();
+                    //_circleBody.LinearVelocity = ConvertUnits.ToSimUnits(new Vector2((float)(DiffDist.X / match), (float)(DiffDist.Y / match)));
+                    
+                    //Text = ClickPos.ToString() + "\n" + Distance.ToString() + " " + match.ToString() + "\n" + ((float)(ClickPos.X / match)).ToString() + " " + ((float)(ClickPos.Y / match)).ToString(); //loc.Position.ToString() + "Vids: " + match.ToString() + "V raz: " + (match / 84).ToString() + "\n" + "Nklic:" + new Vector2((float)(loc.Position.X / (match / 84)), (float)(loc.Position.Y / (match / 84))).ToString();
+                    //////////////
                 }
                     
             }
            
             // TODO: Add your update logic here
             //_world.Step((float)gameTime.ElapsedGameTime.TotalMilliseconds * 0.001f);
-            _world.Step(0.033333f);//оновлюємо світ
+            b._world.Step(0.033333f);//оновлюємо світ
             base.Update(gameTime);
         }
 
@@ -202,23 +173,24 @@ namespace PhysExp
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            
             // TODO: Add your drawing code here
             spriteBatch.Begin();
+            b.DrawBall(spriteBatch);
+            /////////////
+            //spriteBatch.Draw(_circleSprite, ConvertUnits.ToDisplayUnits(_circleBody.Position), null, Color.White, ConvertUnits.ToDisplayUnits(_circleBody.Rotation), ConvertUnits.ToDisplayUnits(_circleCenter), 1f, SpriteEffects.None, 0f);
+            ////                спрайт            позиція тіла                             обертання тіла     центер текстури(зміщуємо верхній лівий кут в центр)
 
-            spriteBatch.Draw(_circleSprite, ConvertUnits.ToDisplayUnits(_circleBody.Position), null, Color.White, ConvertUnits.ToDisplayUnits(_circleBody.Rotation), ConvertUnits.ToDisplayUnits(_circleCenter), 1f, SpriteEffects.None, 0f);
-            //                спрайт            позиція тіла                             обертання тіла     центер текстури(зміщуємо верхній лівий кут в центр)
-
-            //spriteBatch.Draw(_circleSprite, ConvertUnits.ToDisplayUnits(_circleBody2.Position), null, Color.White, ConvertUnits.ToDisplayUnits(_circleBody2.Rotation), ConvertUnits.ToDisplayUnits(_circleCenter), 1f, SpriteEffects.None, 0f);
-            spriteBatch.DrawString(Font, Text, new Vector2(100, 0), Color.Black);
-            spriteBatch.Draw(_1, ConvertUnits.ToDisplayUnits(_circleBody.Position - _1Center), Color.White);
-            spriteBatch.Draw(_1, ConvertUnits.ToDisplayUnits(_circleBody.Position -  new Vector2(_1Center.X - rad, _1Center.Y)), Color.Black);
-            spriteBatch.Draw(_1, ConvertUnits.ToDisplayUnits(_circleBody.Position - new Vector2(_1Center.X, _1Center.Y - rad)), Color.Black);
-            spriteBatch.Draw(_1, ConvertUnits.ToDisplayUnits(_circleBody.Position - new Vector2(_1Center.X + rad, _1Center.Y)), Color.Black);
-            spriteBatch.Draw(_1, ConvertUnits.ToDisplayUnits(_circleBody.Position - new Vector2(_1Center.X, _1Center.Y + rad)), Color.Black);
-            //spriteBatch.DrawString(Font, borderBody.Position.ToString(), ConvertUnits.ToDisplayUnits(new Vector2(borderBody.Position.X, borderBody.Position.Y)), Color.Black);
-            //spriteBatch.DrawString(Font, _circleBody.Position.ToString(), ConvertUnits.ToDisplayUnits( new Vector2(_circleBody.Position.X, _circleBody.Position.Y)), Color.Red);
-            spriteBatch.Draw(_circleSprite, (new Vector2(-(_circleSprite.Width/2), -(_circleSprite.Width/2) )), Color.White);
+            
+            //spriteBatch.DrawString(Font, Text, new Vector2(100, 0), Color.Black);
+            //spriteBatch.Draw(_1, ConvertUnits.ToDisplayUnits(_circleBody.Position - _1Center), Color.White);
+            //spriteBatch.Draw(_1, ConvertUnits.ToDisplayUnits(_circleBody.Position -  new Vector2(_1Center.X - rad, _1Center.Y)), Color.Black);
+            //spriteBatch.Draw(_1, ConvertUnits.ToDisplayUnits(_circleBody.Position - new Vector2(_1Center.X, _1Center.Y - rad)), Color.Black);
+            //spriteBatch.Draw(_1, ConvertUnits.ToDisplayUnits(_circleBody.Position - new Vector2(_1Center.X + rad, _1Center.Y)), Color.Black);
+            //spriteBatch.Draw(_1, ConvertUnits.ToDisplayUnits(_circleBody.Position - new Vector2(_1Center.X, _1Center.Y + rad)), Color.Black);
+            
+            //spriteBatch.Draw(_circleSprite, (new Vector2(-(_circleSprite.Width/2), -(_circleSprite.Width/2) )), Color.White);
+            /////////////////
             spriteBatch.End();
             
             base.Draw(gameTime);
